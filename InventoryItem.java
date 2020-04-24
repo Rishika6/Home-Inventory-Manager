06d0dfd93837563e086164b28e5765154bd742b3
Rishika Wadhera <53297570+Rishika6@users.noreply.github.com>
2020-04-24 20:48:49 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,14 @@
+
+public class InventoryItem 
+{
+	public String description;
+	public String location;
+	public boolean marked;
+	public String serialNumber;
+	public String purchasePrice;
+	public String purchaseDate;
+	public String purchasedLocation;
+	public String note;
+	public String photoFile;
+
+}
